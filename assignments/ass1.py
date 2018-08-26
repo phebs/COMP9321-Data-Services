c61995ca7adda86c6a13ae79407c827070d9e858
phebs <z5088051@zmail.unsw.edu.au>
2018-08-27 04:55:57 +1000

    Fixing it up

--- assignments/ass1.py
+++ assignments/ass1.py
@@ -25,6 +25,12 @@ def print_dataframe(dataframe, print_column=True, print_rows=True):
             line+= ",".join([str(row[column]) for column in dataframe])
             print(line)
 
+def print_series(dataframe):
+    line_index = ",".join([str(key) for key in dataframe.index])
+    line_row = ",".join([str(values) for values in dataframe])
+    print(line_index)
+    print(line_row)
+
 
 def print_full(x):
     pd.set_option('display.max_rows', len(x))
@@ -52,64 +58,84 @@ def fix_index(line):
 
 
 def question_1():
+    print("*********************************************")
+    print("Question 1")
+    print("*********************************************")
     df1 = pd.read_csv('Olympics_dataset1.csv', index_col=0, skiprows=1)
     df2 = pd.read_csv('Olympics_dataset2.csv', index_col=0, skiprows=1)
-    # df1.rename(columns={list(df1)[0]:'col'}, inplace=True)
+    column_rename = {"Number of Games the country participated in_x" : "no_Games_Country_Participated_Summer", 
+                    "Gold_x":"Gold_Summer",
+                    "Silver_x":"Silver_Summer",
+                    "Bronze_x":"Bronze_Summer",
+                    "Total_x":"Total_Summer",
+                    "Number of Games the country participated in_y" : "no_Games_Country_Participated_Winter",
+                    "Gold_y":"Gold_Winter",
+                    "Silver_y":"Silver_Winter",
+                    "Bronze_y":"Bronze_Winter",
+                    "Total_y":"Total_Winter",
+                    "Number of Games the country participated in.1":"no_Games_Country_Participated_Total",
+                    "Gold.1":"Gold_Total",
+                    "Silver.1":"Silver_Total",
+                    "Bronze.1":"Bronze_Total",
+                    "Total.1":"Total_Medals"
+                    }
     dmerged=pd.merge(df1,df2,how='inner',left_index=True,right_index=True)
-    dmerged.rename(index=fix_index,inplace=True)
+    dmerged.rename(columns=column_rename,inplace=True)
+    # dmerged.rename(index=fix_index,inplace=True)
     print_table(dmerged)
     print_dataframe(dmerged.head(5))
     return dmerged
 
 
 def question_2(dmerged):
-    print("***************")
+    print("*********************************************")
     print("Question 2")
-    print("***************")
-    #index has been placed to couutry already in the first question
-    print(dmerged.index[0])
+    print("*********************************************")
+    #index has been placed to country already in the first question
+    series = dmerged.iloc[0]
+    print_series(series)
     return dmerged
 
 def question_3(dmerged):
-    print("***************")
+    print("*********************************************")
     print("Question 3")
-    print("***************")
+    print("*********************************************")
     dmerged.drop('Rubish',axis=1,inplace=True)
     print_dataframe(dmerged.head(5))
     return dmerged
 
 def question_4(dmerged):
-    print("***************")
+    print("*********************************************")
     print("Question 4")
-    print("***************")
+    print("*********************************************")
     dmerged.dropna(inplace=True)
     print_dataframe(dmerged.tail(10))
     return dmerged
 
 def question_5(dmerged):
-    print("***************")
+    print("*********************************************")
     print("Question 5")
-    print("***************")
+    print("*********************************************")
+    dmerged = cleansing(dmerged)
     #Loading Summer Olympic games
     # df1 = pd.read_csv('Olympics_dataset1.csv', index_col=0, skiprows=1)
-    dGold= dmerged['Gold_x'].apply(lambda x: x.replace(',',''))
-    dGold = dGold.astype(float)
-    # dGold=dmerged['Gold_x']
+    dGold=dmerged['Gold_Summer']
     dGold = dGold[:(len(dGold) - 1)]
     max_df=dGold.idxmax()
-    print(max_df)
+    print(str(max_df) + " contains " + str(dGold.loc[max_df]) + " Gold medals")
+    return dmerged
 
 
 def question_6(dmerged):
-    print("***************")
+    print("*********************************************")
     print("Question 6")
-    print("***************")
-    dGoldS = dmerged['Gold_x'].apply(lambda x: x.replace(',',''))
-    dGoldW= dmerged['Gold_y'].apply(lambda x: x.replace(',',''))
+    print("*********************************************")
+    dGoldS = dmerged['Gold_Summer']
+    dGoldW= dmerged['Gold_Winter']
     dGoldS = dGoldS[:(len(dGoldS) - 1 )]
     dGoldW = dGoldW[:(len(dGoldW)) - 1 ]
-    max_df = (dGoldS.astype(float) - dGoldW.astype(float)).abs().idxmax()
-    print(max_df)
+    max_df = (dGoldS - dGoldW).abs().idxmax()
+    print(max_df + "has the highest difference between Summer and Winter Gold medals")
 '''
 TO DO:
 Fix the filter
@@ -118,43 +144,40 @@ cleans Prooperly
 Show the top 5 countries and not the total
 '''
 def question_7(dmerged):
-    print("***************")
+    print("*********************************************")
     print("Question 7")
-    print("***************")
-    print(dmerged.columns)
+    print("*********************************************")
     #Fix the columns and data 
-    dmerged['Total.1']=  dmerged['Total.1'].apply(lambda x: x.replace(',',''))
-    dmerged['Total.1']= dmerged['Total.1'].astype(float)
-    dsorted = dmerged.sort_values(by='Total.1',axis=0,ascending=False,)
+    # dmerged['Total.1']=  dmerged['Total.1'].apply(lambda x: x.replace(',',''))
+    # dmerged['Total.1']= dmerged['Total.1'].astype(float)
+    dsorted = dmerged.sort_values(by='Total_Medals',axis=0,ascending=False,)
     print_table(dsorted)
-    dsorted= dsorted.drop(index='Totals')
-    print(dsorted.index)
-    print("--------- Top 10 Rows ---------")
-    print_dataframe(dsorted.head(10))
-    print("--------- Bottom 10 Rows ---------")
-    print_dataframe(dsorted.tail(10))
+    # dsorted= dsorted.drop(index='Totals')
+    print("--------- Top 5 Rows ---------")
+    print_dataframe(dsorted.head(5))
+    print("--------- Bottom 5 Rows ---------")
+    print_dataframe(dsorted.tail(5))
     return dsorted
 
 def question_8(dsorted):
-    print("***************")
+    print("*********************************************")
     print("Question 8")
-    print("***************")
-    dsorted = cleansing(dsorted)
+    print("*********************************************")
+    dsorted.rename(index=fix_index,inplace=True)
     dftop = dsorted.head(10)
-    ax= dftop.plot.barh(y=['Total_x','Total_y'],stacked=True, title='Medals for Winter and Summer Games',figsize=(5,5))
+    ax= dftop.plot.barh(y=['Total_Summer','Total_Winter'],stacked=True, title='Medals for Winter and Summer Games')
     ax.legend(["Summer Games","Winter Games"])
-    ax.set_aspect(aspect='auto')
     plt.show()
 
 def question_9(dmerged):
-    print("***************")
+    print("*********************************************")
     print("Question 9")
-    print("***************")
-    dmerged = cleansing(dmerged)
+    print("*********************************************")
+    dmerged.rename(index=fix_index,inplace=True)
     countries = ['United States','Australia','Great Britain','Japan','New Zealand']
     Arow = dmerged.loc[countries]
     # ax = plt.axes()
-    ax = Arow.plot.bar(y=["Gold_y","Silver_y","Bronze_y"],title="Winter Games",color=['#4671be','#eb7c3e','#a3a3a3'],rot=0)
+    ax = Arow.plot.bar(y=["Gold_Winter","Silver_Winter","Bronze_Winter"],title="Winter Games",color=['#4671be','#eb7c3e','#a3a3a3'],rot=0)
     ax.legend(["Gold","Silver","Bronze"])
     # ax.set_position(pos='bottom')
     # ax.set_xticklabels(ax.get_xticklabels(),rotation=90)
@@ -170,7 +193,7 @@ if __name__ == '__main__':
     dmerged = question_2(dmerged)
     dmerged = question_3(dmerged)
     dmerged = question_4(dmerged)
-    question_5(dmerged)
+    dmerged = question_5(dmerged)
     question_6(dmerged)
     dsorted = question_7(dmerged)
     question_8(dsorted)
