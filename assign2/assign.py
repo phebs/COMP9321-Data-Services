5458c5ba2fe91379ec70bdeccd11df6fd435ed80
phebs <z5088051@zmail.unsw.edu.au>
2018-09-20 13:51:20 +1000

    Completed assignment

--- assign2/assign.py
+++ assign2/assign.py
@@ -17,6 +17,7 @@ class database(object):
         # Removes the unncessary header
         # data = json.load(f)[1]
         # Retrieves the header for the header collection
+
         header = data[0].get("indicator")
         indicator = header.get("id")
         value = header.get("value")
@@ -64,7 +65,7 @@ class database(object):
             c.insert(records)
 
 
-    def read_from(self,query):
+    def read_from(self,query,abbreviate):
         """
         :param mongo_host: Mongodb server address
         :param mongo_port: Mongodb server port number
@@ -75,11 +76,34 @@ class database(object):
         db = self.client[self.db]
         db.authenticate('phb', 'COMP9321')
         c = db[self.collection]
-        result = list(c.find(query))
+
+        ignore = {'_id':0}
+        result = list(c.find(query,ignore))
+        # print(result)
         if len(result) < 1:
             return None
         else:
+            #Removes the useless header information
             result=result[0]
+            if abbreviate:
+                result_return = {
+                    #May change the integer conversion
+                    "location": self.collection + "/" + str(result.get('collection_id')),
+                    "collection_id": result.get("collection_id"),
+                    "creation_time": result.get("creation_time"),
+                    "indicator": result.get("indicator"),
+                }
+            else:
+                result_return = result
+            return result_return
+
+    def read_all(self):
+        db = self.client[self.db]
+        db.authenticate('phb', 'COMP9321')
+        c = db[self.collection]
+        result_list=list()
+        response = list(c.find().sort("collection_id"))
+        for result in response:
             result_return = {
                 #May change the integer conversion
                 "location": self.collection + "/" + str(result.get('collection_id')),
@@ -87,7 +111,28 @@ class database(object):
                 "creation_time": result.get("creation_time"),
                 "indicator": result.get("indicator"),
             }
-            return result_return
+            result_list.append(result_return)
+        return result_list
+
+    def queryYD(self,query,collection_id,year,country):
+        db = self.client[self.db]
+        db.authenticate('phb', 'COMP9321')
+        c = db[self.collection]
+        result = list(c.find(query))
+        result = result[0]
+        #retrieves the first one that satisfies the criteria
+        for response in result.get("entries"):
+            if response.get("date") == year and response.get("country") == country:
+                result_return = {
+                    "collection_id": result.get("collection_id"),
+                    "indicator": result.get("indicator"),
+                    "country": response.get("country"),
+                    "year": response.get("date"),
+                    "value": response.get("value")
+                }
+                return result_return
+        return None
+
 
     def delete(self,query):
         db = self.client[self.db]
@@ -96,22 +141,32 @@ class database(object):
         response = c.delete_one(query)
         #prints how many documents it deleted
         #TODO can use to write a message
-        print(response.deleted_count)
+        # print(response.deleted_count)
         return response.deleted_count
 
 
-# if __name__ == '__main__':
-#
-#     db_name = 'my-database'
-#     mongo_port = 27872
-#     mongo_host = 'ds127872.mlab.com'
-#
-#     collection = 'Economic Indicator'
-#
-#     print("Writing into the mongodb")
-#     write_in_mongodb(df, mongo_host, mongo_port, db_name, collection)
-#
-#     print("Querying the database")
-#     dataframe = read_from_mongodb(mongo_host, mongo_port, db_name, collection)
-#
-#     print_dataframe(dataframe)
+    def country_print(self,collection_id,year,decide,no):
+        db = self.client[self.db]
+        db.authenticate('phb', 'COMP9321')
+        c = db[self.collection]
+
+        query = {"collection_id" : collection_id}
+        holder = list()
+        result = list(c.find(query))
+        result = result[0]
+        #goes through all the entries to find which entries have the same date
+        for response in result.get("entries"):
+            if response.get("date") == year:
+                holder.append(response)
+        if (decide == "top"):
+            ifreverse=True
+        else:
+            ifreverse=False
+        #Sorts the values
+        result_sort=sorted(holder,key=lambda x: (x.get("value") is not None, x.get("value")),reverse=ifreverse)
+
+        if (len(result_sort) >= no):
+            result_sort=result_sort[:no]
+
+        return result_sort
+
--- assign2/client.py
+++ assign2/client.py
@@ -1,15 +1,17 @@
 from flask import Flask, request
-from flask_restplus import Resource, Api, fields
+from flask_restplus import Resource, Api, fields, reqparse
 import json
 import requests
 from assign import *
+import re
+
 app = Flask(__name__)
 api = Api(app)
 
 
 # The following is the schema of Book
 #TODO needs to be changed
-book_model = api.model('Book', {
+Economic_model = api.model('EconInd', {
     'Flickr_URL': fields.String,
     'Publisher': fields.String,
     'Author': fields.String,
@@ -19,14 +21,28 @@ book_model = api.model('Book', {
     'Place_of_Publication': fields.String
 })
 
+parser = reqparse.RequestParser()
+parser.add_argument('q')
+
+
 @api.route('/EconIndicator')
 class ToPut(Resource):
+    @api.expect(200,'Successfully printed all Economic Indicators')
+    @api.doc(description="Retrieves all Economic Indicators")
+    def get(self):
+        response = db.read_all()
+        return response,200
+
+    @api.expect(201,"Successfully added into database")
+    @api.expect(200,"Already on database")
+    @api.expect(404,"Indicator ID doesn't exist")
+    @api.doc(description="Adds particular indicator ID to the database")
     def post(self):
         content = request.json
         url_input = content.get("indicator_id")
         query = {"indicator": url_input}
         #Checks if it is in the database
-        response = db.read_from(query)
+        response = db.read_from(query,True)
         #If it is already on the database, return it
         if response is not None:
             print("Already found on the database")
@@ -41,20 +57,71 @@ class ToPut(Resource):
         data = json.loads(response.content)
         if (len(data)) == 2:
             db.createPayload(data[1])
-            response = db.read_from(query)
+            response = db.read_from(query,True)
             return response,201
         else:
-            api.abort(400,"This indicator id doesn't exist")
+            api.abort(404,"This indicator id doesn't exist")
             # if the data doesn't exist
 
 @api.route('/EconIndicator/<int:collection_id>')
-class ToDelete(Resource):
+class ToCollectionID(Resource):
+    @api.expect(200, "Successful showing")
+    @api.doc(description="Lists the details of a particular collection id")
     def get(self,collection_id):
+        query = {"collection_id": collection_id}
+        abbreviate = False
+        #TODO if collection id doesn't exist
+        response = db.read_from(query,abbreviate)
+        # print(response)
+        return response,200
+
+    @api.expect(200,"Succesfully deleted")
+    @api.expect(404,"Collection ID doesn't exist")
+    @api.doc(description="Deletes data according to collection ID")
+    def delete(self,collection_id):
         query = { "collection_id": collection_id }
         notime = db.delete(query)
         if notime < 1:
             return {collection_id: "Doesn't exist"},404
-        return {collection_id: "is now deleted"},200
+        return {"message": "Collection = " + str(collection_id) + " is removed from the database!"},200
+
+@api.route('/EconIndicator/<int:collection_id>/<year>')
+class queryTopBot(Resource):
+    @api.expect(404,"Either number/collection id/query doesn't exist")
+    @api.expect(200,"Retrieved the certain number of countries")
+    @api.expect(parser, validate=True)
+    @api.doc(description="Finds the top-bottom results")
+    def get(self, collection_id, year,):
+        args = parser.parse_args()
+        query = args.get('q')
+        # query = request.args.get('q')
+        print(query)
+        match = re.match(r'(top|bottom)([0-9]+)', query, re.M | re.I)
+        if match:
+            decide = match.group(1)
+            no = int(match.group(2))
+            if no > 0 and no <= 100:
+                result = db.country_print(collection_id, year, decide, no)
+                #TODO check to make sure that the year and collection ID exists
+                return result,200
+            else:
+                api.abort(404, "Number is not within the range")
+        else:
+            api.abort(404, "Query is not correct")
+
+
+@api.route('/EconIndicator/<int:collection_id>/<year>/<country>')
+class queryYrD(Resource):
+    @api.doc(description="Searches for a country that fits the criteria")
+    @api.expect(200,"Successfully found")
+    @api.expect(400,"CollectionID/Year/Country doesn't exist")
+    def get(self,collection_id,year,country):
+        query = {"collection_id": collection_id}
+        result = db.queryYD(query,collection_id,year,country)
+        if result:
+            return result,200
+        else:
+            api.abort(400,"This year and country doesn't exist")
 
 
 
